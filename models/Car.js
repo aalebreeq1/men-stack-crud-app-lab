f21c791b791bfc14228c134e8f3ca34b906b3914
aalebreeq1 <abdullaebreeq1@gmail.com>
2026-09-07 09:52:46 +0300

    feat: create car schema and model

---
+++ models/Car.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+
+const carSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true,
+        trim: true
+    },
+    type:{
+        type:String,
+        enum: ['Sedan' ,"SUV" , "Truck" , "Coupe", "Crossover"]
+    },
+    make:{
+        type:String,
+        required: true,
+        trim: true
+    },
+    model:{
+        type:String,
+        required: true,
+        trim: true
+    },
+    year:{
+        type:Number,
+        required: true,
+    },
+    MSRP:{
+        type:Number,
+        required:true,
+        min: 0 
+
+    },
+    Dealer:{
+        type:String,
+        required: true,
+        trim: true
+    },
+    engine_type:{
+        type:String,
+        enum:['Petrol', 'Diesel', 'Electric', 'Hybrid']
+    }
+
+},{timestamps: true})
+
+const Car = mongoose.model('Car', carSchema)
+module.exports = Car
